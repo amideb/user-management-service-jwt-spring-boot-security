8f6da1cc42c3fdb56fd29403e2dcbdf414e870a8
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-25 12:26:31 +0530

    user-fields-updated

--- src/main/java/com/debrup/usermanagementbackend/domain/User.java
+++ src/main/java/com/debrup/usermanagementbackend/domain/User.java
@@ -1,6 +1,7 @@
 package com.debrup.usermanagementbackend.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class User implements Serializable {
 
@@ -11,6 +12,14 @@ public class User implements Serializable {
     private String userName;
     private String password;
     private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles; //ROLE_USER {read, delete, update, create} , ROLE_ADMIN
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
 
 
 }
