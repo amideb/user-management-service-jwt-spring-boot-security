f53a187483aa0fed1604a0f3f214298bcd6d1c3b
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-25 12:39:21 +0530

    entity annotation added

--- src/main/java/com/debrup/usermanagementbackend/domain/User.java
+++ src/main/java/com/debrup/usermanagementbackend/domain/User.java
@@ -2,9 +2,16 @@ package com.debrup.usermanagementbackend.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String userId;
     private String firstName;
