b537625656aecefe63b6e85f72cd07eae27ae23e
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-25 12:06:15 +0530

    spring boot application initialized

---
+++ src/main/java/com/debrup/usermanagementbackend/UserManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.debrup.usermanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementBackendApplication.class, args);
+    }
+
+}
