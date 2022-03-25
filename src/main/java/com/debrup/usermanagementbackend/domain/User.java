267cb5c9b09db465ff7ba7853f4702286433ccc0
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-25 12:18:09 +0530

    fields added to user

--- src/main/java/com/debrup/usermanagementbackend/domain/User.java
+++ src/main/java/com/debrup/usermanagementbackend/domain/User.java
@@ -3,4 +3,14 @@ package com.debrup.usermanagementbackend.domain;
 import java.io.Serializable;
 
 public class User implements Serializable {
+
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String email;
+
+
 }
