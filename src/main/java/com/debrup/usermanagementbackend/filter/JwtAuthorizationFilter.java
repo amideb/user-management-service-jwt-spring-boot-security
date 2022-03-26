197968fa84adc8dd5076446a2c3dce8b0ca1be9d
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-26 11:38:36 +0530

    JwtAuthorizationFilter added

---
+++ src/main/java/com/debrup/usermanagementbackend/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,56 @@
+package com.debrup.usermanagementbackend.filter;
+
+import com.debrup.usermanagementbackend.constant.SecurityConstant;
+import com.debrup.usermanagementbackend.utility.JWTTokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static com.debrup.usermanagementbackend.constant.SecurityConstant.*;
+import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpStatus.*;
+
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private JWTTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(JWTTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    //token is valid, user is valid
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD)){
+            response.setStatus(OK.value());
+        }else{
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if(authorizationHeader==null || authorizationHeader.startsWith(TOKEN_PREFIX)){
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = authorizationHeader.substring(TOKEN_PREFIX.length());
+            String username = jwtTokenProvider.getSubject(token);
+            if(jwtTokenProvider.isTokenValid(username,token) && SecurityContextHolder.getContext().getAuthentication()==null){
+                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+                Authentication authentication = jwtTokenProvider.getAuthentication(username, authorities, request);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else{
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+}
