b23ef234eb75dfde66e3baccb307efaafaabc4f1
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-26 12:14:30 +0530

    access-entry-point added

---
+++ src/main/java/com/debrup/usermanagementbackend/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package com.debrup.usermanagementbackend.filter;
+
+import com.debrup.usermanagementbackend.constant.SecurityConstant;
+import com.debrup.usermanagementbackend.domain.HttpResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Locale;
+
+import static com.debrup.usermanagementbackend.constant.SecurityConstant.*;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        HttpResponse httpResponse =
+                new HttpResponse(FORBIDDEN.value(),
+                        FORBIDDEN,
+                        FORBIDDEN.getReasonPhrase().toUpperCase(),
+                        FORBIDDEN_MESSAGE);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.value());
+
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+
+
+    }
+}
