2bdc2a4bd03a72be2c982f72e3e3d9d1af7a6aeb
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-28 12:29:07 +0530

    entry point added

--- src/main/java/com/debrup/usermanagementbackend/filter/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/debrup/usermanagementbackend/filter/JwtAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,7 @@ import static com.debrup.usermanagementbackend.constant.SecurityConstant.*;
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Component
 public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
 
     @Override
