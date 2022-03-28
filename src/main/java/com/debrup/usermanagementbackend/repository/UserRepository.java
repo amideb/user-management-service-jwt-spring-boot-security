7f6cbce0349d17854637f77a16b825fb0f9027b9
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-28 12:27:40 +0530

    user repo unchanged

--- src/main/java/com/debrup/usermanagementbackend/repository/UserRepository.java
+++ src/main/java/com/debrup/usermanagementbackend/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.debrup.usermanagementbackend.repository;
 
 import com.debrup.usermanagementbackend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
