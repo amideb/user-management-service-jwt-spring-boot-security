6b032fd2be8b32af0eebf3f9ab17e46d930aa27b
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-28 15:45:43 +0530

    timeStamp added

--- src/main/java/com/debrup/usermanagementbackend/domain/HttpResponse.java
+++ src/main/java/com/debrup/usermanagementbackend/domain/HttpResponse.java
@@ -1,8 +1,13 @@
 package com.debrup.usermanagementbackend.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
+
 public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "India/Kolkata")
+    private Date timeStamp;
     private int httpStatusCode; //200 - OK, 400   ,500 -server error,
     private HttpStatus httpStatus;
     private String reason;
@@ -12,10 +17,19 @@ public class HttpResponse {
     }
 
     public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp = new Date();
         this.httpStatusCode = httpStatusCode;
         this.httpStatus = httpStatus;
         this.reason = reason;
         this.message = message;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
     }
 
     public int getHttpStatusCode() {
