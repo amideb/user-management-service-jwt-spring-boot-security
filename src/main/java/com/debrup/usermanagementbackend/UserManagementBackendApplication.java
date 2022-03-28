a204b6516d4be068210548c6a61214bbddf0e14e
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-28 12:28:38 +0530

    bean-added

--- src/main/java/com/debrup/usermanagementbackend/UserManagementBackendApplication.java
+++ src/main/java/com/debrup/usermanagementbackend/UserManagementBackendApplication.java
@@ -2,6 +2,11 @@ package com.debrup.usermanagementbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UserManagementBackendApplication {
@@ -9,5 +14,12 @@ public class UserManagementBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserManagementBackendApplication.class, args);
     }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 }
