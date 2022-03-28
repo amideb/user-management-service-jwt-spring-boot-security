a5b330659cee25a1489d44392e5bde8aa67a0f9f
Debrup Mondal <debrup.official.365@gmail.com>
2022-03-28 15:46:08 +0530

    repo updated

--- src/main/java/com/debrup/usermanagementbackend/repository/UserRepository.java
+++ src/main/java/com/debrup/usermanagementbackend/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.debrup.usermanagementbackend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findUserByUsername(String username);
+    User findUserByUserName(String username);
 
     User findUserByEmail(String email);
 
